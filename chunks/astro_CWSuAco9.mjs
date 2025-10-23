4aa5cc8f26a7c402ec93b962ebe61421bf96d916
axDxs <addiesinha03@gmail.com>
2025-10-23 04:08:34 -0500

    Updates

--- chunks/astro_CWSuAco9.mjs
+++
@@ -1,3 +0,0 @@
-import 'kleur/colors';
-import './astro/server_BpWU_tsO.mjs';
-import 'clsx';
--- chunks/content-modules_DrWN_wy-.mjs
+++
@@ -1,4 +0,0 @@
-const contentModules = new Map([
-["src/content/pages/elements.mdx", () => import('./elements_X9up8ia7.mjs')]]);
-
-export { contentModules as default };
